d29634b82f6506c19178cebd9a9c052d051c7f6a
Emanuele Carbone <emanuele.carbone@a-tono.com>
2013-01-29 15:14:34 +0100

    fixed bug on meetme form

--- meetme/forms.py
+++ meetme/forms.py
@@ -14,7 +14,13 @@ class MeetmeForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(MeetmeForm, self).__init__(*args, **kwargs)
-        self.initial['room'] = random.randint(10000,99999)  
+
+        if kwargs.has_key('instance'):
+            instance = kwargs['instance']            
+            self.initial['room'] = instance.room  
+        else:
+            self.initial['room'] = random.randint(10000,99999)  
+
         self.fields['start_time'].widget = widgets.AdminSplitDateTime()
         self.fields['duration'].widget = forms.Select(choices=[(d, d) for d in range (1,11)])
         self.fields['description'].widget = forms.TextInput(attrs={'size':'60'})
